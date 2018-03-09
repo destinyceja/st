178b17a604e718bb0aec56ddc7fc160923465c78
destinyceja <33135802+destinyceja@users.noreply.github.com>
2018-03-09 14:29:10 -0800

    Create st.rb

---
+++ st.rb
@@ -0,0 +1,37 @@
+=begin
+
+TASK 1: Define a function that has 5 parameters (four notes and one sleep time). Hit "Run" and nothing should play.
+
+TASK 2: Call your function and give it five arguments (the four notes on lines 16, 18, 20, & 22 plus the sleep time). Hit run and you should hear four notes play.
+
+TASK 3: Call your function twice in a live_loop so the entire Stranger Things Theme repeats.
+
+=end
+
+use_bpm 160
+use_synth :saw
+
+#define your function with parameters
+define :play_eighth_notes do
+  
+  # First set of eighth notes
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+play_eighth_notes
